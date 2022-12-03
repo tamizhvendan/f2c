fe21f3c118bfe7b937200b18e2dffbc468373e9d
Tamizhvendan S <tamizh88@gmail.com>
2022-12-03 11:48:01 +0530

    feat: add html templating logic [#1]

--- src/f2c/extension/ring_response.clj
+++ src/f2c/extension/ring_response.clj
@@ -1,6 +1,12 @@
 (ns f2c.extension.ring-response
-  (:require [clojure.data.json :as json]))
+  (:require [clojure.data.json :as json]
+            [rum.core :as rum]))
 
 (defn json [data]
   {:body (json/write-str data)
    :headers {"Content-Type" "application/json"}})
+
+(defn html [content]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str "<!doctype html>" (rum/render-static-markup content))})
--- src/f2c/web/app/individual/index.clj
+++ src/f2c/web/app/individual/index.clj
@@ -1,6 +1,12 @@
 (ns f2c.web.app.individual.index 
   (:require [f2c.web.app.individual.auth :as individual-auth]
-            [ring.util.response :as rr]))
+            [f2c.extension.ring-response :as rr]))
 
 (defn handler [req]
-  (rr/response (str "Hi " (:individual/name (individual-auth/current-individual req)))))
+  (rr/html
+   [:html 
+    [:head 
+     [:title "Dashboard"]]
+    [:body 
+     [:div 
+      [:p (str "Hi " (:individual/name (individual-auth/current-individual req)))]]]]))
